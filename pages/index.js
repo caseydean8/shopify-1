107571b30f7205eed1cb2feed25f89dacc8c6e8a
Casey Carroll <caseydean8@gmail.com>
2020-05-23 13:44:49 -0400

    App is connected to store

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+// import { Layout, Page, TextStyle } from '@shopify/polaris';
+
+const Index = () => (
+  <div>
+    <p>Sample app using React and Next.js</p>
+  </div>
+  // <Page>
+  //   <Layout>
+  //   <TextStyle variation="positive">
+  //     Sample app using React and Next.js
+  //   </TextStyle>
+  //   </Layout>
+  // </Page>
+);
+
+export default Index;
